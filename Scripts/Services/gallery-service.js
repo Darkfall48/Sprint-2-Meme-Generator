b1686544f264f9f88d13ab979ccd871747e1e848
Darkfall48 <sidney@sebban.be>
2022-12-15 09:38:15 +0200

    Downloading Meme is now working

--- Scripts/Services/gallery-service.js
+++ Scripts/Services/gallery-service.js
@@ -28,54 +28,21 @@ function _createImages() {
       _createImage('./Images/Gallery/1.jpg', ['funny', 'cat']),
       _createImage('./Images/Gallery/2.jpg', ['funny', 'cat']),
       _createImage('./Images/Gallery/3.jpg', ['funny', 'cat']),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/4.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/5.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/6.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/7.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/8.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/9.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/10.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/11.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/12.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/13.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/14.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
-      // _createImage(
-      //   'https://github.com/Darkfall48/Sprint-2-Meme-Generator/blob/main/Images/Gallery/15.jpg?raw=true',
-      //   ['funny', 'cat']
-      // ),
+      _createImage('./Images/Gallery/4.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/5.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/6.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/7.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/8.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/9.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/10.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/11.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/12.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/13.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/14.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/15.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/16.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/17.jpg', ['funny', 'cat']),
+      _createImage('./Images/Gallery/18.jpg', ['funny', 'cat']),
     ]
     console.log('Gallery Created')
   }
